9921571ab19605cd874cc3cdcc89abb008e98f10
Aravind Samala <52457571+thyaravind@users.noreply.github.com>
2021-06-03 17:43:25 -0400

    certificate Org

---
+++ integration/tools_certificates/organization.js
@@ -0,0 +1,79 @@
+import pool from "../../config/db_connection.js";
+//const { v4: uuidv4 } = require('uuid');
+
+
+async function fetchAllOrgs() {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let sql_resp = await pool.query('SELECT * from certificateOrganizations');
+            resolve(sql_resp);
+        }
+        catch (err) {
+            reject(err);
+        }
+
+    });
+
+}
+
+
+async function fetchOrgByName(orgName) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let sql_resp = await pool.query('SELECT * from certificateOrganizations where name = ?', orgName);
+            resolve(sql_resp);
+        }
+        catch (err) {
+            reject(err);
+        }
+
+    });
+
+}
+
+async function addOrg(orgName) {
+    //orgID = uuidv4()
+    return new Promise(async (resolve, reject) => {
+        try {
+            let sql_resp = await pool.query('INSERT INTO certificateOrganizations (organizationID,name) VALUES (?,?)', [orgID, orgName]);
+            resolve(sql_resp);
+        }
+        catch (err) {
+            reject(err);
+        }
+
+    });
+
+}
+
+async function fetchOrg(ID) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            //use stored proc to fetch certificate Organization
+            let sql_resp = await pool.query('SELECT * from organizations.orgs where orgID = ?', ID);
+            resolve(sql_resp);
+        }
+        catch (err) {
+            reject(err);
+        }
+
+    });
+
+}
+
+async function updateOrg(org) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let sql_resp = await pool.query('UPDATE organizations.orgs set ? where orgID = ?', [org, org.orgID]);
+            resolve(sql_resp);
+        }
+        catch (err) {
+            reject(err);
+        }
+
+    });
+
+}
+
+
+export { fetchAllOrgs, fetchOrgByName, fetchOrg, addOrg, updateOrg }
